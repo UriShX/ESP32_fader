a151344d28d690443b952ec1d723b4e23d14cefd
UriShX <usdogi@gmail.com>
2019-06-04 10:12:01 +0300

    dataview uint8

--- esp32_fader.js
+++ esp32_fader.js
@@ -93,7 +93,7 @@ function handleNotifications(event) {
   }
   console.log('> ' + a.join(' '));
 
-	slider.value = new DataView(value);//value.getUint8(0);
+	slider.value = new DataView(value).getUint8(0);//value.getUint8(0);
 	output.innerHTML = slider.value;
   console.log(slider.value + "\t" + typeof(slider.value));
 }
